4b645eafa14316df294edc9d01e7ab0f20281ce5
Iqmii <bilousdavid1910@gmail.com>
2025-03-14 16:21:40 +0100

    Update authRouter.js

--- authRouter.js
+++ authRouter.js
@@ -1,16 +1,13 @@
-const Router = require('express');
-const router = new Router();
-const controller = require('./authController')
+// routes/authRouter.js
 
+const express = require('express');
+const router = express.Router();
+const controller = require('./authController');
 
+// Припустимо, що ваш authController має відповідні методи:
 router.post('/registration', controller.registration);
-router.post('/verificationCode', controller.vereficationCode);
+router.post('/verificationCode', controller.verificationCode); // або vereficationCode, якщо так називається метод
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
-
-if  (router.post) { 
-    console.log("router.post('/registration', controller.registration);");
-}
-
 
 module.exports = router;
